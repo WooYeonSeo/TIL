708e631c64c9be478e94f165d21d7fa6b7cdd723
unknown <vantovan7414@gmail.com>
2019-04-10 18:17:42 +0900

    다이나믹 9095 - 수 더하기 문제

---
+++ TIL/src/dynamic/dynamicAddNum_9095.java
@@ -0,0 +1,22 @@
+package dynamic;
+/*
+ *  n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
+ *  input : 정수 n(n은 양수이며 11보다 작다)
+ *  output : 갯수 
+ */
+import java.util.Scanner;
+public class dynamicAddNum_9095 {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int dp[] = new int[11];
+    // 점화식
+    for (int i = 0; i < n; i++) {
+      
+    }
+    
+  }
+
+}
